50398b71cb9c8a4838b1b755b2bb6c0bab2bc238
Shuki1220 <holly19941220@gmail.com>
2026-08-12 10:21:03 +0900

    Add files via upload

--- qmk_firmware/keyboards/hatsu_one/keymaps/vial/config.h
+++ qmk_firmware/keyboards/hatsu_one/keymaps/vial/config.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0xae, 0x37, 0x79, 0xf0, 0x94, 0xa1, 0x34, 0x08}
-#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_TAP_DANCE_ENTRIES 32
 #define VIAL_COMBO_ENTRIES 8
 #define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
