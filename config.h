fbf0267671b954cf19370dbd41dc7e796bf1a7ea
Shuki1220 <holly19941220@gmail.com>
2026-09-12 07:45:25 +0900

    Add files via upload

---
+++ config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xae, 0x37, 0x79, 0xf0, 0x94, 0xa1, 0x34, 0x08}
+#define VIAL_TAP_DANCE_ENTRIES 40
+#define VIAL_COMBO_ENTRIES 10
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
+#define DYNAMIC_KEYMAP_MACRO_COUNT 40
